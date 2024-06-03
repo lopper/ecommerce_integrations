f3af4a9389695fdb95b6b6b298c818bd24c9bb87
Patrick <patricklo@gmail.com>
2024-06-03 23:36:54 +0000

    zencart: add po_date

--- ecommerce_integrations/zencart/order.py
+++ ecommerce_integrations/zencart/order.py
@@ -96,6 +96,7 @@ def create_sales_order(zencart_order, setting, company=None):
 				"doctype": "Sales Order",
 				"naming_series": setting.sales_order_series or "SO-Zencart-",
 				"po_no": "ZN-" + zencart_order.get("order_id"),
+				"po_date": getdate(zencart_order.get("date_purchased")) or nowdate(),
 				ORDER_ID_FIELD: str(zencart_order.get("order_id")),
 				"customer": customer,
 				"transaction_date": getdate(zencart_order.get("date_purchased")) or nowdate(),
